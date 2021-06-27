acc6136cfdab91ceb96df940efacb4e2ca202693
ligus <75608937+ligusx@users.noreply.github.com>
2021-06-27 17:22:06 +0800

    Add files via upload

--- github_hosts.py
+++ github_hosts.py
@@ -38,7 +38,29 @@ def main():
     f.write("codeload.github.com\n")
     f.write("status.github.com\n")
     f.write("training.github.com\n")
+    f.write("central.github.com\n")
+    f.write("desktop.githubusercontent.com\n")
+    f.write("github.map.fastly.net\n")
+    f.write("github.global.ssl.fastly.net\n")
+    f.write("favicons.githubusercontent.com\n")
+    f.write("user-images.githubusercontent.com\n")
+    f.write("avatars.githubusercontent.com\n")
+    f.write("avatars0.githubusercontent.com\n")
+    f.write("avatars1.githubusercontent.com\n")
+    f.write("avatars2.githubusercontent.com\n")
+    f.write("avatars3.githubusercontent.com\n")
+    f.write("avatars4.githubusercontent.com\n")
+    f.write("avatars5.githubusercontent.com\n")
+    f.write("github-cloud.s3.amazonaws.com\n")
+    f.write("github-com.s3.amazonaws.com\n")
+    f.write("github-production-release-asset-2e65be.s3.amazonaws.com\n")
+    f.write("github-production-user-asset-6210df.s3.amazonaws.com\n")
+    f.write("github-production-repository-file-5c1aeb.s3.amazonaws.com\n")
+    f.write("githubstatus.com\n")
+    f.write("github.community\n")
+    f.write("media.githubusercontent.com\n")
     f.write("api.github.com\n")
+    f.write("github.io\n")
     f.close()
 
 if __name__ == "__main__":
@@ -47,8 +69,6 @@ if __name__ == "__main__":
 def main():
     f = open('%s/hosts' % file_path,'w')
     f.write("127.0.0.1 localhost\n")
-    f.write("\n")
-    f.write("#github accelerate\n")
     f.write("\n")
     f.write("# GitHub Start\n")
     f.close()
@@ -60,7 +80,7 @@ def main():
                 result.write(ip.strip('\n') + " " + host)
 
     f = open('%s/hosts' % file_path,'a')
-    f.write("\n# GitHub End\n")
+    f.write("# GitHub End\n")
     f.close()
 
 if __name__ == "__main__":
